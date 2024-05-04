c5dbbd724c2378132a79fbb29e8ec2c5e9568218
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-04 14:37:35 -0400

    password generator
    - fix JS of the light mode and dark mode

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
+//Arrays//
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+//booleans//
 let isDarkMode = true
 let isLightMode = false
+
+//DOM variables//
 let generateBtn = document.getElementById("generate-btn")
 let password1 = document.getElementById("password1")
 let password2 = document.getElementById("password2")
 let title1 = document.getElementById("title1")
 let colorBtn = document.getElementById("color-btn")
 
-
+//functions//
 function changeColor(){
-    if(isLightMode === true){
-        isDarkMode = true
-        isLightMode = false
+    if(isDarkMode === true){
         document.body.style.backgroundColor = "#ECFDF5";
         colorBtn.style.backgroundColor ="#10B981";
         title1.style.color = "black";
-        colorBtn.textContent = "dark Mode"
-    } else if(isDarkMode === true) {
+        colorBtn.textContent = "light mode";
         isDarkMode = false
         isLightMode = true
+    } else if(isLightMode === true) {
         document.body.style.backgroundColor = "#1F2937";
         colorBtn.style.backgroundColor = "#273549";
         title1.style.color = "white";
-        colorBtn.textContent = "light Mode";
+        colorBtn.textContent = "dark mode"
+        isDarkMode = true
+        isLightMode = false
        }
-    
 }
 
 function generatePasswords(){
